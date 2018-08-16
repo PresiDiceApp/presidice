3dfb9a839b7dd3afcaff4fce71858ece0899e4bb
Luis Cobian <coby10@gmail.com>
2018-08-16 13:24:42 -0500

    color: verde

--- presidice/index.php
+++ presidice/index.php
@@ -7,7 +7,7 @@
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="../bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
     </head>
-<body role="document" style="margin:20px;color:gray">
+<body role="document" style="margin:20px;color:green">
 	<div class="container theme-showcase" role="main">
     	<div class="">
         	<h1>El presi dice...</h1>
