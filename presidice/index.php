ad93b3ac1333c82fa2fc96b4e033bca2aa16f965
Luis Cobian <coby10@gmail.com>
2018-08-16 13:21:59 -0500

    modifca el color: cobian

--- presidice/index.php
+++ presidice/index.php
@@ -7,7 +7,7 @@
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="../bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
     </head>
-<body role="document" style="margin:20px">
+<body role="document" style="margin:20px;color:gray">
 	<div class="container theme-showcase" role="main">
     	<div class="">
         	<h1>El presi dice...</h1>
