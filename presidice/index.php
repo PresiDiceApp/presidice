96ebec14efee83a8939f4b704a8c47de95ef6ecd
rerinconf77 <rrincon@itstequila.edu.mx>
2018-08-17 11:37:15 -0500

    Se modifico el titulo de la linea 6 del index.php

--- presidice/index.php
+++ presidice/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    	<title>Frases celebres del presi</title>
+    	<title>Frases Celebres de El Presi Master</title>
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="../bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
     </head>
